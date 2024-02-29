17fde52bd1b0257e30a4dc1e4d644e70f7a28fc6
Segerlu <111815126+Segerlu@users.noreply.github.com>
2024-02-29 16:11:50 -0500

    1. navigate to the backend and use the command 'npm install' 2. then run the command 'nodemon server.js' 3. in your index.js, you will need to connect to the new backend with the url 'http://localhost:9999'.  just fetch like you would an api

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors')
+let app = express();
+let PORT = 9999;
+app.use(express.json())
+app.use(cors())
+
+//GET
+app.get('/', (req, res) => {
+    const url = 'https://zenquotes.io/api/quotes'
+
+    console.log('fetching quotes')
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        res.status(200).send(data)
+    })
+    .catch(console.log)
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
